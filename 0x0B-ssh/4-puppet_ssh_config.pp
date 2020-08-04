6de435a15f8b49d437ab03fcbbaf4555db5891e7
AlejoCasti <acastiblanco8@gmail.com>
2020-08-04 20:26:12 +0000

    [ADD advanced task]

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -6,3 +6,9 @@ file_line { 'change passwordAuthentication':
   line   => '    PasswordAuthentication no',
   match  => '^PasswordAuthentication'
 }
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
+}
