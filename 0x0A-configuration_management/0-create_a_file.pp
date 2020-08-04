e63d8d0017d56847eddf8f7bb747ccaf321ac4f5
AlejoCasti <acastiblanco8@gmail.com>
2020-08-04 00:14:34 +0000

    [ADD] the files was created without documentation

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
+#create a file with content
+
 file { '/tmp/holberton':
   ensure  => file,
   path    => '/tmp/holberton',
-  mode    => '0744',
   owner   => 'www-data',
+  mode    => '0744',
   group   => 'www-data',
   content => 'I love Puppet'
 }
