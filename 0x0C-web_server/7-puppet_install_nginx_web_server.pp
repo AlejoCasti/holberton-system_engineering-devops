1c6f656ef72b1de7c6d5bb3cf95fbe41991f2b1a
AlejoCasti <acastiblanco8@gmail.com>
2020-08-06 02:58:32 +0000

    [ADD] advanced task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 #Setting server
 
-exec { 'Server':
+exec { 'install nginx':
   provider => 'shell',
-  command  => 'apt-get -y update ; apt-get -y install nginx ; echo "Holberton School" > /var/www/html/index.html ;  sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n\trewrite ^\/redirect_me http:\/\/google.com permanent;/" /etc/nginx/sites-available/default ; service nginx start'
+  command  => 'apt-get -y update ; apt-get -y install nginx'
 }
+
+
+exec { 'create index.html':
+  provider => 'shell',
+  command  => 'echo "Holberton School" > /var/www/html/index.html'
+}
+
+exec { '301 value and start server':
+  provider => 'shell',
+  command  => 'sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n\trewrite ^\/redirect_me http:\/\/google.com permanent;/" /etc/nginx/sites-available/default ; service nginx start'
+}
+
