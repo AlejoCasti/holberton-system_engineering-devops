c375de5cb73ed4b0cbec4cf7a1c0eec357b0a0ff
AlejoCasti <acastiblanco8@gmail.com>
2020-08-15 00:33:52 +0000

    [ADD] advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -7,5 +7,6 @@ package { 'nginx':
 
 exec { 'add header':
   provider => 'shell',
-  command  => 'sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-available/default;service nginx start'
+  command  => 'sed -i "s/root \/var\/www\/html;/root \/var\/www\/html;\n\tadd_header X-Served-By \$hostname;/"
+ /etc/nginx/sites-available/default;service nginx start'
 }
