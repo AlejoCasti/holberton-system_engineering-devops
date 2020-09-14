203dd24a336aa1376fb34c45d51cb7873b95a1bb
AlejoCasti <acastiblanco8@gmail.com>
2020-09-14 23:16:07 +0000

    [ADD] 1 task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' get info from API '''
+
+
+if __name__ == '__main__':
+    ''' get conection to API '''
+    import csv
+    import json
+    import requests
+    from sys import argv
+
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(argv[1]))
+    employee = json.loads(r.text)
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    lista = json.loads(r.text)
+    todo_list = [i for i in lista if i['userId'] == int(argv[1])]
+    todo = [i for i in todo_list if i['completed']]
+
+    with open('{}.csv'.format(argv[1]), 'w') as f:
+        writer = csv.writer(f)
+        for row in todo_list:
+            writer.writerow([argv[1], employee['name'],
+                             row['completed'], row['title']])
