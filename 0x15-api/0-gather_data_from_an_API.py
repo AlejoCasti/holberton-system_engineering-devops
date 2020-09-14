28210d690cbf7d87c188c8199a1757ed2f0134fd
AlejoCasti <acastiblanco8@gmail.com>
2020-09-14 22:49:29 +0000

    [ADD] project

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' get info from API '''
+
+
+if __name__ == '__main__':
+    ''' get conection to API '''
+    from sys import argv
+    import json
+    import requests
+
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                     .format(argv[1]))
+    employee = json.loads(r.text)
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    lista = json.loads(r.text)
+    todo_list = [i for i in lista if i['userId'] == int(argv[1])]
+    todo = [i for i in todo_list if i['completed']]
+    print('Employee {} is done with tasks({}/{}):'
+          .format(employee['name'], len(todo), len(todo_list)))
+    for i in todo:
+        print('\t{}'.format(i['title']))
